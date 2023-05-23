448a7ce7e8662d0c08e8aa344c4ed1e8544ce066
Muhammad Usman <50750473+smusman437@users.noreply.github.com>
2023-05-23 15:06:12 +0500

    add declarations and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
+import { TodosListComponent } from './components/todos-list/todos-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    TodoDetailsComponent,
+    TodosListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
