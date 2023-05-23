3bd4082723e2466ea2a8411dfd92090bb7ed3fd2
Muhammad Usman <50750473+smusman437@users.noreply.github.com>
2023-05-23 15:05:15 +0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosListComponent } from './components/todos-list/todos-list.component';
+import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'todos', pathMatch: 'full' },
+  { path: 'todos', component: TodosListComponent },
+  { path: 'todos/:id', component: TodoDetailsComponent },
+  { path: 'add', component: AddTodoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
